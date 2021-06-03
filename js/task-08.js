ee7d56a2e7d0dc2cb92cbdd85e302b1b702df986
chillhaze <65725016+chillhaze@users.noreply.github.com>
2021-06-03 16:05:06 +0300

    js-hw-7.8 first try

--- js/task-08.js
+++ js/task-08.js
@@ -4,6 +4,7 @@ const refs = {
   btnCreate: document.querySelector('button'),
   btnClear: document.querySelector('button[data-action="destroy"]'),
   boxes: document.querySelector('#boxes'),
+  box: document.querySelector('#box'),
 };
 
 refs.btnCreate.classList.add('createBtn');
@@ -15,16 +16,35 @@ refs.btnCreate.addEventListener('click', onCreateBtnClick);
 let amount;
 function onImputChange(event) {
   amount = event.currentTarget.value;
+  // console.log(event.target.value);
   createBoxes(amount);
 }
 
-let markup;
-const createMarkup = function () {
-  markup = '<div id="box"></div>';
-
-  return refs.boxes.insertAdjacentHTML('afterbegin', markup).join('');
-};
+let markup = '<div id="box" class="random"></div>';
+function createBoxes(amount) {
+  for (let i = 1; i < amount; i++) {
+    i = `<div id="box" class="random"></div>`;
+    markup += i;
+  }
+}
 
 function onCreateBtnClick() {
-  return amount * createMarkup;
+  if (amount >= 1) {
+    return refs.boxes.insertAdjacentHTML('beforeend', markup);
+  }
 }
+
+const styles = document.querySelector('style');
+console.log(styles);
+
+const boxStyles = {
+  width: 55 + Math.floor(Math.random() * 256),
+  height: 40 + Math.floor(Math.random() * 86),
+  color: `rgb(${155 + Math.floor(Math.random() * 26)},${
+    5 + Math.floor(Math.random() * 146)
+  },${1 + Math.floor(Math.random() * 69)})`,
+};
+
+const newStyle = `.random {background-color: ${boxStyles.color}; width: ${boxStyles.width}px; height: ${boxStyles.height}px}`;
+
+styles.insertAdjacentHTML('afterbegin', newStyle);
