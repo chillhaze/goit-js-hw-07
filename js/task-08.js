4d7a128c59b88f07af0e88029f669a1c4208c308
chillhaze <65725016+chillhaze@users.noreply.github.com>
2021-06-07 04:39:40 +0300

    workinng on task #8

--- js/task-08.js
+++ js/task-08.js
@@ -1,7 +1,7 @@
 const refs = {
   controls: document.querySelector('#controls'),
   input: document.querySelector('input'),
-  btnCreate: document.querySelector('button'),
+  btnCreate: document.querySelector('[data-action="render"]'),
   btnClear: document.querySelector('button[data-action="destroy"]'),
   boxes: document.querySelector('#boxes'),
   box: document.querySelector('#box'),
@@ -11,40 +11,34 @@ refs.btnCreate.classList.add('createBtn');
 refs.btnClear.classList.add('destroyBtn');
 
 refs.input.addEventListener('input', onImputChange);
-refs.btnCreate.addEventListener('click', onCreateBtnClick);
+refs.btnCreate.addEventListener('click', createBoxes);
+refs.btnClear.addEventListener('click', destroyBoxes);
 
-let amount;
+let inputValue;
 function onImputChange(event) {
-  amount = event.currentTarget.value;
-  // console.log(event.target.value);
-  createBoxes(amount);
+  inputValue = Number(event.currentTarget.value);
+  // console.log(typeof inputValue);
 }
 
-let markup = '<div id="box" class="random"></div>';
 function createBoxes(amount) {
-  for (let i = 1; i < amount; i++) {
-    i = `<div id="box" class="random"></div>`;
-    markup += i;
+  amount = inputValue;
+  console.log(amount);
+
+  let newArr = [];
+  for (let i = 1; i <= amount; i++) {
+    let newBox = document.createElement('div');
+    newBox.style.width = `${50 + Math.floor(Math.random() * 350)}px`;
+    newBox.style.height = `${10 + Math.floor(Math.random() * 35)}px`;
+    newBox.style.backgroundColor = `rgb(${
+      1 + Math.floor(Math.random() * 290)
+    },${1 + Math.floor(Math.random() * 55)},${
+      1 + Math.floor(Math.random() * 111)
+    })`;
+    i = newArr.push(newBox);
   }
+  return refs.boxes.append(...newArr);
 }
 
-function onCreateBtnClick() {
-  if (amount >= 1) {
-    return refs.boxes.insertAdjacentHTML('beforeend', markup);
-  }
+function destroyBoxes() {
+  return refs.boxes.remove('.box');
 }
-
-const styles = document.querySelector('style');
-console.log(styles);
-
-const boxStyles = {
-  width: 55 + Math.floor(Math.random() * 256),
-  height: 40 + Math.floor(Math.random() * 86),
-  color: `rgb(${155 + Math.floor(Math.random() * 26)},${
-    5 + Math.floor(Math.random() * 146)
-  },${1 + Math.floor(Math.random() * 69)})`,
-};
-
-const newStyle = `.random {background-color: ${boxStyles.color}; width: ${boxStyles.width}px; height: ${boxStyles.height}px}`;
-
-styles.insertAdjacentHTML('afterbegin', newStyle);
